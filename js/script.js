c267c4e9640310ed08fa129535ba0ac64980fbe4
Gastón Konstantinides <gastonkons@gmail.com>
2023-02-14 16:59:13 -0300

    feat: copyToClipboard, change var for const/let, an attemp to debounce

--- js/script.js
+++ js/script.js
@@ -1,36 +1,78 @@
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var h3 = document.querySelector("h3");
-var body = document.querySelector("#gradient");
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const resultText = document.querySelector('#resultCode');
+const body = document.querySelector('#background-generator');
+const btnGradient = document.querySelector('#gradient__btn');
+const btnCopy = document.querySelector('#gradient__result__copy');
+let isChanging = false;
+
+color1.addEventListener('input', changeBackground);
+color2.addEventListener('input', changeBackground);
+btnGradient.addEventListener('click', randomBackground, false);
+btnCopy.addEventListener('click', copyToClipboard, false);
+
+function changeBackground(isRandomBackground) {
+
+	if (isChanging && !isRandomBackground) return;
+
+	isChanging = true;
+
+	resultText.classList.add('gradient__result-animate');
+	setTimeout(() => {
+		resultText.classList.remove('gradient__result-animate');
+		isChanging = false
+	}, 1000);
+
+	setTimeout(() => {
+		const bgStyle = getBgStyle()
+		body.style.background = bgStyle;
+		btnGradient.style.background = bgStyle;
+		resultText.textContent = getBgStyleCode(bgStyle);
+	}, 500);
+}
+
+function randomBackground() {
+	if (isChanging) return;
+
+	isChanging = true;
+
+	setTimeout(() => {
+		color1.value = randomRGB();
+		color2.value = randomRGB();
+	}, 500)
+	changeBackground(isChanging);
+}
+
+function randomRGB() {
+	return '#000000'.replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
+}
+
+function getBgStyle() {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`
+}
+
+function getBgStyleCode() {
+	return `background: ${getBgStyle()};`
+}
+
+function copyToClipboard() {
+	document.querySelectorAll('.gradient__result__notification')
+		.forEach(item => item.remove())
+
+	const notification = document.createElement('span');
+	notification.classList.add("gradient__result__notification");
+
+	navigator.clipboard
+		.writeText(getBgStyleCode(getBgStyle()))
+		.then(() => {
+			notification.textContent = "Code copied! 💪";
+		})
+		.catch(() => {
+			notification.textContent = "🔴 We can't copy the code. :(";
+		})
+		.finally(() => {
+			btnCopy.appendChild(notification)
+		})
+}
+
 changeBackground();
-
-color1.addEventListener("input", changeBackground);
-color2.addEventListener("input", changeBackground);
-
-function changeBackground(){
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value + ", " + color2.value + ")";
-	h3.textContent = body.style.background + ";";
-}
-
-var button = document.getElementById("random");
-
-button.addEventListener("click", randomBackground, false);
-
-function randomBackground(){
-	// var colorRGB_1 = randomRGB();
-	// var colorRGB_2 = randomRGB(); 
-	// body.style.background = "linear-gradient(to right, " 
-	// + colorRGB_1 + ", " + colorRGB_2 + ")";
-	// h3.textContent = body.style.background + ";";
-	color1.value = randomRGB();
-	color2.value = randomRGB();
-	changeBackground();
-}
-
-function randomRGB(){
- // from StackOverFlow
-	// return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	return "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-}
